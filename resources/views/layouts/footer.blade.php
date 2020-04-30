302cb0078d1080bf539fec5ac437bf2f7eea0041
Austinove <bryanovicaustenove@gmail.com>
2020-04-30 21:24:44 +0300

    css fixes in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -20,7 +20,7 @@
 					<div class="col-md-4">
 						<div class="footer_links footer_column" style="padding-top: 0px">
 							<div class="footer_about_text">
-								<h4>Useful Links</h4>
+								<h4 class="white_color">Useful Links</h4>
 							</div>
 							<ul>
 								<li><a href="#">FAQ</a></li>
@@ -34,7 +34,7 @@
 					</div>
 					<div class="col-md-5">
 						<div class="footer_about_text" style="overflow: hidden">
-							<h4>Map</h4>
+							<h4 class="white_color">Map</h4>
 							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.1120479635842!2d34.17774463306795!3d1.078086451248954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1778b66c9a9f2c71%3A0xac02e03f3a00a25c!2sMbale%20People&#39;s%20Clinic%20(Vaena)!5e0!3m2!1sen!2sus!4v1588141719639!5m2!1sen!2sus" width="600" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 					</div>
 				</div>
